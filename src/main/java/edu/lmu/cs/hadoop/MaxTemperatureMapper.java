dca6bca3f60283f01f4f4df503540405a59b3ecc
Andrew Won <andyjwon@gmail.com>
2013-05-13 17:47:51 -0700

    Reformatted code to be prettier

--- src/main/java/edu/lmu/cs/hadoop/MaxTemperatureMapper.java
+++ src/main/java/edu/lmu/cs/hadoop/MaxTemperatureMapper.java
@@ -7,27 +7,24 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class MaxTemperatureMapper
-  extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
-  enum Temperature {
-    MALFORMED
-  }
-
-  private NcdcRecordParser parser = new NcdcRecordParser();
-
-  @Override
-  public void map(LongWritable key, Text value, Context context)
-      throws IOException, InterruptedException {
-
-    parser.parse(value);
-    if (parser.isValidTemperature()) {
-      int airTemperature = parser.getAirTemperature();
-      context.write(new Text(parser.getYear()), new IntWritable(airTemperature));
-    } else if (parser.isMalformedTemperature()) {
-      System.err.println("Ignoring possibly corrupt input: " + value);
-      context.getCounter(Temperature.MALFORMED).increment(1);
+    enum Temperature {
+        MALFORMED
     }
-  }
+
+    private NcdcRecordParser parser = new NcdcRecordParser();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        parser.parse(value);
+        if (parser.isValidTemperature()) {
+            int airTemperature = parser.getAirTemperature();
+            context.write(new Text(parser.getYear()), new IntWritable(airTemperature));
+        } else if (parser.isMalformedTemperature()) {
+            System.err.println("Ignoring possibly corrupt input: " + value);
+            context.getCounter(Temperature.MALFORMED).increment(1);
+        }
+    }
 }
-// ^^ MaxTemperatureMapperV5
--- src/main/java/edu/lmu/cs/hadoop/MaxTemperatureReducer.java
+++ src/main/java/edu/lmu/cs/hadoop/MaxTemperatureReducer.java
@@ -1,25 +1,21 @@
 package edu.lmu.cs.hadoop;
-//cc MaxTemperatureReducerV1 Reducer for maximum temperature example
+
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-// vv MaxTemperatureReducerV1
-public class MaxTemperatureReducer
-  extends Reducer<Text, IntWritable, Text, IntWritable> {
+public class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-  @Override
-  public void reduce(Text key, Iterable<IntWritable> values,
-      Context context)
-      throws IOException, InterruptedException {
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException,
+            InterruptedException {
 
-    int maxValue = Integer.MIN_VALUE;
-    for (IntWritable value : values) {
-      maxValue = Math.max(maxValue, value.get());
+        int maxValue = Integer.MIN_VALUE;
+        for (IntWritable value : values) {
+            maxValue = Math.max(maxValue, value.get());
+        }
+        context.write(key, new IntWritable(maxValue));
     }
-    context.write(key, new IntWritable(maxValue));
-  }
 }
-// ^^ MaxTemperatureReducerV1
